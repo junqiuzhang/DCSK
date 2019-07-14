79e8bf6e225792ebf56fb7bb880f4f13cfe8670f
zhangjunqiu <zhangjunqiu3@gmail.com>
2019-07-14 20:35:39 +0800

    fix(main)画不同扩频系数的图

--- main.m
+++ main.m
@@ -1,38 +1,41 @@
 % clear all;clc;close all;
 
 %% 参数配置
-Bit_n=10000;% 码元数量
 fc=30000; % 载波频率
 fd=1000; % 频偏
 fs=96000;% 采样频率
 nSamp=1/fs;% 采样间隔
-Bit=single(sign(randn(1,Bit_n)));% 码元
-L=100;% 扩频增益
-Nsample=Bit_n*L*2;% 扩频后采样点数量
+Bit_n=10000;% 码元数量
+Bit=single(sign(randn(1,Bit_n)));% 待发送码元
 
 %% 参数配置显示及效验
-Vb=fs/L;
 fprintf('\r');
 fprintf('通信参数\r');
 fprintf('--------------\r');
-fprintf('采样频率: %g Hz\n',fs);
-fprintf('通信速率: %g bit/s\n',Vb);
-fprintf('扩频因子: %g \n',L);
-fprintf('载频过采样: %g \n',fs/L);
 
-%% 混沌信号生成
-u=logistic_map(Nsample);
 
-%% DCSK FM-DCSK调制解调
 hold on
 grid on
 Eb_N0=0:1:20;
-axis([0 Eb_N0(end) 10^-4 10^0]);
-
-BER_DCSK=DCSK(Bit_n,L,Bit,u);
-semilogy(Eb_N0,BER_DCSK,'r','LineWidth',2);
-%     BER_FM_DCSK=FM_DCSK(Bit_n,L,Bit,u,fc,fs,fd);
-%     semilogy(Eb_N0,BER_FM_DCSK,'b','LineWidth',2);
+% axis([0 Eb_N0(end) 10^-4 10^-0.3]);
+color=['r','g','b','y','k'];
+% color=['r','g'];
+for i = 1:5
+    L=i*6;% 扩频增益
+    Nsample=Bit_n*L*2;% 采样点数量
+    % 混沌信号生成
+    u=logistic_map(Nsample);
+    
+    % DCSK FM-DCSK调制解调
+%     BER_DCSK=DCSK(Bit_n,L,Bit,u);
+%     semilogy(Eb_N0,BER_DCSK,color(i),'LineWidth',2);
+    BER_FM_DCSK=FM_DCSK(Bit_n,L,Bit,u,fc,fs,fd);
+    semilogy(Eb_N0,BER_FM_DCSK,color(i),'LineWidth',2);
+end
+legend('L=6','L=12','L=18','L=24','L=30');
+xlabel('扩频增益');
+ylabel('误码率');
+% legend('DCSK','FM_DCSK')
 
 % %% 调制
 % Modulated_signal=Chaotic_modulate(Bit_n,L,Bit,u);
@@ -59,6 +62,4 @@ semilogy(Eb_N0,BER_DCSK,'r','LineWidth',2);
 % semilogy(Eb_N0,BER,'yo-','LineWidth',2)
 
 %% plot
-
-% fre_spec(u,fs);
 
